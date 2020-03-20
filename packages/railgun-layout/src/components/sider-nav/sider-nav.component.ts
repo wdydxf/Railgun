1d3de7ff266beb479ae9def2db2751becb606856
NoDocCat <nodoccat@outlook.com>
2020-03-20 20:13:28 +0800

    chore(cli): 调整生成器配置, 将less作为默认预处理器

--- packages/railgun-layout/src/components/sider-nav/sider-nav.component.ts
+++ packages/railgun-layout/src/components/sider-nav/sider-nav.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'rg-sider-nav',
   templateUrl: './sider-nav.component.html',
-  styleUrls: ['./sider-nav.component.css']
+  styleUrls: ['./sider-nav.component.less']
 })
 export class SiderNavComponent implements OnInit {
 
