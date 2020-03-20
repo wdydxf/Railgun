a01e0ccb2ad2b65ec096d86f9891576bb3c05646
NoDocCat <nodoccat@outlook.com>
2020-03-20 20:44:10 +0800

    feat(railgun-layout): 完成导航菜单组件的一级菜单渲染

--- packages/railgun-layout/src/components/sider-nav/sider-nav.component.ts
+++ packages/railgun-layout/src/components/sider-nav/sider-nav.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Menu } from '../../interface';
 
 @Component({
   selector: 'rg-sider-nav',
   templateUrl: './sider-nav.component.html',
   styleUrls: ['./sider-nav.component.less']
 })
-export class SiderNavComponent implements OnInit {
+export class SiderNavComponent {
+  @Input() menus: Menu[] = [];    // 菜单数据
+  @Input() collapsed = false;     // 是否折叠菜单
 
-  constructor() {
+  /**
+   * 判定是否为外部链接
+   * @param link  链接地址
+   */
+  isExternalLink(link: string) {
+    return (/(\w):\/\//).test(link);
   }
-
-  ngOnInit(): void {
-  }
-
 }
--- packages/railgun-layout/src/railgun-layout.module.ts
+++ packages/railgun-layout/src/railgun-layout.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { SiderNavComponent } from './components/sider-nav/sider-nav.component';
+import { NzIconModule, NzMenuModule } from 'ng-zorro-antd';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [
     SiderNavComponent
   ],
-  imports: [],
+  imports: [
+    NzMenuModule,
+    CommonModule,
+    RouterModule,
+    NzIconModule
+  ],
   exports: [
     SiderNavComponent
   ]
