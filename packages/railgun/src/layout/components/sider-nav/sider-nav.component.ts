ed731105ef444f779944544b31fc02eb23e437fd
NoDocCat <nodoccat@outlook.com>
2020-03-27 20:31:20 +0800

    feat(sider-nav): 当路由匹配时自动展开菜单

--- packages/railgun/src/layout/components/sider-nav/sider-nav.component.ts
+++ packages/railgun/src/layout/components/sider-nav/sider-nav.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Location } from '@angular/common';
 import { Menu } from '../../interface';
 
 @Component({
@@ -6,9 +7,15 @@ import { Menu } from '../../interface';
   templateUrl: './sider-nav.component.html',
   styleUrls: ['./sider-nav.component.less']
 })
-export class SiderNavComponent {
+export class SiderNavComponent implements OnChanges {
   @Input() menus: Menu[] = [];    // 菜单数据
   @Input() collapsed = false;     // 是否折叠菜单
+  @Input() autoExpand = true;     // 是否根据路由自动展开菜单
+
+  constructor(
+    private location: Location
+  ) {
+  }
 
   /**
    * 判定是否为外部链接
@@ -16,5 +23,37 @@ export class SiderNavComponent {
    */
   isExternalLink(link: string) {
     return (/(\w):\/\//).test(link);
+  }
+
+  /**
+   * 检查菜单项, 若其子项与路由匹配则展开自身
+   * @param menu 要检查的菜单项
+   */
+  checkMenuItemExpand(menu: Menu): boolean {
+    const path = this.location.path();
+
+    if (menu.children) {
+      menu.open = false;  // 默认关闭
+      menu.children.forEach(v => {
+        // 检查子项
+        if (!v.children && v.link && path.startsWith(v.link)) {
+          menu.open = true;
+        }
+
+        // 对于多级菜单递归向下检查
+        if (v.children && this.checkMenuItemExpand(v)) {
+          menu.open = true;
+        }
+      });
+    }
+
+    return !!menu.open;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // 执行菜单项检查
+    if (this.autoExpand && changes.menus) {
+      this.menus.forEach(v => this.checkMenuItemExpand(v));
+    }
   }
 }
