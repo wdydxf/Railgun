96243571e855edfdbf7d8a0250470d70e8513b4b
NoDocCat <nodoccat@outlook.com>
2020-03-27 19:44:05 +0800

    feat(railgun): 归一化打包入口, 处理打包过程中依赖的配置问题

--- packages/railgun/index.ts
+++ packages/railgun/index.ts
@@ -1,2 +1,7 @@
-// only for dev
-export * from './src/packages';
+/*
+ * Public API Surface of railgun
+ */
+
+export * from './src/railgun.module';
+export * from './src/axios';
+export * from './src/layout';
--- packages/railgun/src/packages.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of railgun
- */
-
-export * from './railgun.module';
-export * from './axios';
-export * from './layout';
